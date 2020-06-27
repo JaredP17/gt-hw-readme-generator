e254a06e374b9012770eea98ef133b8dbb3dd4a1
Jared Porter <jaredp17@me.com>
2020-06-26 22:59:53 -0400

    Required fs and inquirer. Added pseudocode.

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+// Require packages
+const fs = require("fs");
+const inquirer = require("inquirer");
+
 // array of questions for user
 const questions = [
-
+  // https://www.npmjs.com/package/inquirer#questions
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
 function init() {
-
+  // Use inquirer package
 }
 
 // function call to initialize program
