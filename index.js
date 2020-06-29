c647c6b678fe273b4e58bec9ee307eb05e20b9d4
Jared Porter <jaredp17@me.com>
2020-06-29 15:01:51 -0400

    Code refactoring.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const { title } = require("process");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
@@ -74,73 +75,7 @@ function init() {
   // Use inquirer package
   inquirer
     .prompt(questions)
-    .then((data) => {
-      // console.log(data);
-      const {
-        gitHub,
-        email,
-        title,
-        description,
-        license,
-        installCmd,
-        testCmd,
-        usage,
-        contributing,
-      } = data;
-      return `# ${title.toLowerCase().split(" ").join("-")}
-
-![GitHub license](https://img.shields.io/badge/license-${license
-        .split(" ")
-        .join("_")}-blue.svg)
-
-## Description
-
-${description}
-
-## Table of Contents
-
-* [Installation](#installation)
-
-* [Usage](#usage)
-
-* [License](#license)
-
-* [Contributing](#contributing)
-
-* [Tests](#tests)
-
-* [Questions](#questions)
-
-## Installation
-
-To install necessary dependencies, run the following command:
-
-\`\`\`
-${installCmd}
-\`\`\`
-
-## Usage
-
-${usage}
-
-## License
-
-This project is licensed under the ${license} license.
-
-## Contributing
-
-${contributing}
-
-## Tests
-
-\`\`\`
-${testCmd}
-\`\`\`
-
-## Questions
-
-If you have any questions about the repo, open an issue or contact me directly at ${email}. You can find more of my work at [jaredp17](https://github.com/${gitHub}/).`;
-    })
+    .then((data) => generateMarkdown(data))
     .then((res) => {
       writeToFile("./generated/README.md", res);
     })
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,70 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const {
+    gitHub,
+    email,
+    title,
+    description,
+    license,
+    installCmd,
+    testCmd,
+    usage,
+    contributing,
+  } = data;
 
-`;
+  return `# ${title.toLowerCase().split(" ").join("-")}
+
+![GitHub license](https://img.shields.io/badge/license-${license
+    .split(" ")
+    .join("_")}-blue.svg)
+
+## Description
+
+${description}
+
+## Table of Contents
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+\`\`\`
+${installCmd}
+\`\`\`
+
+## Usage
+
+${usage}
+
+## License
+
+This project is licensed under the ${license} license.
+
+## Contributing
+
+${contributing}
+
+## Tests
+
+\`\`\`
+${testCmd}
+\`\`\`
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at ${email}. You can find more of my work at [jaredp17](https://github.com/${gitHub}/).`;
 }
 
 module.exports = generateMarkdown;
